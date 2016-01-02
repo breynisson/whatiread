b1025df28e94706b28301972a82a3cee8194a76f
breynisson <bjorgvin.reynisson@gmail.com>
2016-01-02 00:16:34 +0000

    First commit for library app

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+
+var adminRouter = express.Router();
+
+var router = function (nav) {
+
+    adminRouter.route('/addBooks')
+        .get(function (req, res) {
+            res.send('inserting books');
+        });
+
+    return adminRouter;
+
+};
+
+module.exports = router;
