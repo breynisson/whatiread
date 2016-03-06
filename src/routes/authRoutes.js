74365afb712e4c05f1a76cdcbc6b3b0afe8d589f
breynisson <bjorgvin.reynisson@gmail.com>
2016-03-06 17:29:31 +0000

    Set authRoute to use Postgres and Massive.js.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,22 +2,23 @@
 
 var express = require('express');
 var authRouter = express.Router();
-var mongodb = require('mongodb').MongoClient;
+var massive = require('massive');
 var passport = require('passport');
 
 var router = function () {
     authRouter.route('/signUp')
         .post(function (req, res) {
             console.log(req.body);
-            var url = 'mongodb://localhost:27017/libraryApp';
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('users');
+            var url = 'postgres://localhost/postgres';
+            massive.connect({connectionString:url}, function (err, db) {
+                var collection = db.users;
                 var user = {
                     username: req.body.userName,
                     password: req.body.password
                 };
                 collection.insert(user, function (err, results) {
-                    req.login(results.ops[0], function () {
+                    console.log(results);
+                    req.login(results, function () {
                         res.redirect('/auth/profile');
                     });
                 });
@@ -39,6 +40,7 @@ var router = function () {
             next();
         })
         .get(function (req,res) {
+            console.log(req.user);
             res.json(req.user);
         });
     return authRouter;
