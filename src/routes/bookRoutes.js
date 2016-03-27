2f2076ded8ab523a93a402a6b4e419caee11ee4f
breynisson <bjorgvin.reynisson@gmail.com>
2016-03-27 13:41:54 +0000

    Remove unused code.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,8 +2,6 @@
 
 var express = require('express');
 var bookRouter = express.Router();
-var mongodb = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
 
 var router = function (nav) {
 
