76fae272f2284ebec8ac5bc763260d4d990cabc9
breynisson <bjorgvin.reynisson@gmail.com>
2016-05-05 20:28:53 +0000

    A few changes to get CircleCi to run the mocha tests.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var mocha = require('mocha');
 var request = require('supertest');
 var app = require('../app.js').getApp;
 
-mocha.describe('GET', function () {
-    mocha.it('expects HTTP response 200', function (done) {
+describe('GET', function () {
+    it('expects HTTP response 200', function (done) {
         request(app)
             .get('/')
             .expect(200, done);
